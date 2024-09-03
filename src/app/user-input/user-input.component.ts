0ebe1d117bd32363095f78effd0b545b72b963b7
PAVI-SARAVANAN <paviithra261@gmail.com>
2024-09-03 19:55:38 +0530

    Update User-Input Component

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ControlComponent } from "../shared/control/control.component";
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ControlComponent],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  enteredIniInvestment = '0';
+  enteredAnnInvestment = '0';
+  enteredExpReturn = '10';
+  enteredDuration = '1';
+
+  OnSubmit(){
+    console.log(this.enteredAnnInvestment + 'Submitted');
+  }
 
 }
